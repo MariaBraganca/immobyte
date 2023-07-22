112cf8e0ef3ef15a7df17d205f0cc5cb837a6851
Maria Braganca <maria@chartmogul.com>
2023-07-22 18:42:13 +0200

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './static/templates/*.html', './auctions/templates/auctions/*.html'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
