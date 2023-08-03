6264fff396aee34c5614c025938a81c9933cabc3
Maria Braganca <maria@chartmogul.com>
2023-08-03 18:13:58 +0200

    detach code from configuration

--- immobyte/settings.py
+++ immobyte/settings.py
@@ -22,12 +22,12 @@ import os
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
 
 # Application definition
@@ -82,15 +82,17 @@ WSGI_APPLICATION = 'immobyte.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'db',
-        'USER': os.environ["DB_USER"],
-        'NAME': os.environ["DB_NAME"],
-        'PASSWORD': os.environ["DB_PASSWORD"],
-        'PORT': os.environ["DB_PORT"]
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.{}'.format(
+             os.getenv('DATABASE_ENGINE', 'postgresql')
+         ),
+         'NAME': os.getenv('DATABASE_NAME', 'postgres'),
+         'USER': os.getenv('DATABASE_USERNAME', 'postgres'),
+         'PASSWORD': os.getenv('DATABASE_PASSWORD', 'password'),
+         'HOST': os.getenv('DATABASE_HOST', '127.0.0.1'),
+         'PORT': os.getenv('DATABASE_PORT', 5432)
+     }
+ }
 
 
 # Password validation
