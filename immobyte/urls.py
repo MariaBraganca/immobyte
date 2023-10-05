53735acfa5383bd7da87dfe0445aa451b77259f8
Maria Braganca <maria@chartmogul.com>
2023-10-05 13:25:47 +0200

    add login page

--- immobyte/urls.py
+++ immobyte/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auctions.urls'))
+    path('', include('auctions.urls')),
+    path('login', LoginView.as_view())
 ]
 
 if settings.DEBUG:
