16e62f92374acfc99bd83434b227eae1ce5db9f4
Maria Braganca <maria@chartmogul.com>
2023-11-08 10:08:00 +0100

    use environment variables for gunicorn conf

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,9 +1,11 @@
+import os
+
 # gunicorn.conf.py
-bind = "0.0.0.0:8000"
-workers = 3
+bind = '0.0.0.0'.format(os.getenv('DJANGO_BACKEND_PORT', '8000'))
+workers = int(os.getenv('GUNICORN_LOG_WORKERS', '3'))
 # Access log - records incoming HTTP requests
 accesslog = "/opt/immobyte/log/gunicorn/immobyte.access.log"
 # Error log - records Gunicorn server goings-on
 errorlog = "/opt/immobyte/log/gunicorn/immobyte.error.log"
 # How verbose the Gunicorn error logs should be
-loglevel = "debug"
+loglevel = os.getenv('GUNICORN_LOG_LEVEL', 'info')
