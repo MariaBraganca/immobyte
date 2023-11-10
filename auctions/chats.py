faef6d09cfc869d5cd9811de320dc9e9734e48fa
Maria Braganca <maria@chartmogul.com>
2023-11-10 07:23:04 +0100

    add assisted chat class to auctions

---
+++ auctions/chats.py
@@ -0,0 +1,56 @@
+from django.contrib.auth.models import User
+from openai import OpenAI
+import os
+
+class AssistedUserChat:
+    """Attempt to model assisted user chats."""
+    
+    def __init__(self, user_id):
+        """Initalize user, client, assistant and thread."""
+        self.user = User.objects.get(pk=user_id)
+        self.client = OpenAI(api_key= os.getenv('OPENAI_API_KEY'))
+        self.assistant = self.client.beta.assistants.create(
+            name = 'Immobyte Assistant',
+            instructions = 'You are a real estate agent. Write and run code to sell real estate property.',
+            tools = [{'type': 'code_interpreter'}],
+            model = 'gpt-4-1106-preview'
+        )
+        self.thread = self.client.beta.threads.create()
+        
+    def call(self, content):
+        """Add message to thread."""
+        self.add_message(content)
+                
+        """Run the assistant."""
+        run = self.run_assistant()
+
+        """Enqueue the assistant's response."""
+        self.retrieve_assistant(run.id)
+        
+    def add_message(self, content):
+        self.client.beta.threads.messages.create(
+            thread_id = self.thread.id,
+            role = 'user',
+            content = content
+        )
+        
+    def run_assistant(self):
+        object = self.client.beta.threads.runs.create(
+            thread_id = self.thread.id,
+            assistant_id = self.assistant.id,
+            instructions = f"Please address the user as {self.user.username}."
+        )
+        return object
+        
+    def retrieve_assistant(self, run_id):
+        object = self.client.beta.threads.runs.retrieve(
+            thread_id = self.thread.id,
+            run_id = run_id
+        )
+        return object
+    
+    def read_thread_messages(self):
+        thread_messages = self.client.beta.threads.messages.list(thread_id = self.thread.id).data
+        
+        [print(tm.content[0].text.value) for tm in thread_messages]
+    
