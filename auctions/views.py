df498c1400b315444bb53a52fea239ba60dd3786
Maria Braganca <maria@chartmogul.com>
2023-11-23 10:32:50 +0100

    fix empty lines

--- auctions/views.py
+++ auctions/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from auctions.models import Auction
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 class AuctionListView(LoginRequiredMixin, ListView):
