d6d173b008c3e1168f11f267ed6c6a31203798d7
Maria Braganca <maria@chartmogul.com>
2023-12-22 08:30:34 +0100

    improve error handling and readability

--- auctions_ai/chats.py
+++ auctions_ai/chats.py
@@ -12,6 +12,7 @@ class AssistedUserChat:
 
     @classmethod
     async def create(cls, user_id):
+        """Initializes an instance of AssistedUserChat asynchronously."""
         self = cls()
         self.client = AsyncOpenAI(max_retries=2, timeout=300.0)
         self.assistant = await self.client.beta.assistants.create(
@@ -28,27 +29,37 @@ class AssistedUserChat:
         return self
 
     async def call(self, content):
-        """Add message to thread."""
-        await self.add_message(content)
+        """Handles chat interaction in order to return a response."""
+        await self.add_user_message(content)
 
-        """Run the assistant."""
+        if not await self.process_assistant_response():
+            return "Immobyte's Assistant failed to process a response. Please try again later."
+
+        response = await self.parse_assistant_response()
+
+        return response if response else "Immobyte's Assistant failed to parse a response. Please try again later."
+
+
+    async def process_assistant_response(self):
         run_assistant = await self.run_assistant()
+        if run_assistant:
+            return await self.check_status_completed(run_assistant.id)
 
-        """Check the run status."""
-        run_status_completed = await self.check_status_completed(run_assistant.id)
+        logger.warning("Unable to process assistant's response.")
+        return False
 
-        """Display the assistant's response."""
-        if run_status_completed:
-            try:
-                thread_messages = await self.read_thread_messages()
-                return thread_messages.data[0].content[0].text.value
-            except Exception as e:
-                logger.warning("Could not return the assistant's message.", e)
-        else:
-            return "Immobyte's Assistant failed to return an answer. Please try again later."
+    async def parse_assistant_response(self):
+        thread_messages = await self.read_thread_messages()
+        try:
+            # TODO's:
+            # Add parser for thread messages.
+            return thread_messages.data[0].content[0].text.value
+        except (IndexError, AttributeError):
+            logger.exception("Unexpected message structure.")
+            return None
 
     @log_openai_error
-    async def add_message(self, content):
+    async def add_user_message(self, content):
         await self.client.beta.threads.messages.create(
             thread_id = self.thread.id,
             role = 'user',
@@ -69,20 +80,22 @@ class AssistedUserChat:
             run_id = run_id
         )
 
-    async def check_status_completed(self, run_id, max_retries=10):
+    # TODO's:
+    # Add an exponential backoff instead of a fixed interval for retries.
+    async def check_status_completed(self, run_id, max_retries = 10, interval = 5):
         retries = 0
         while retries <= max_retries:
+            run = await self.retrieve_assistant(run_id)
             try:
-                run = await self.retrieve_assistant(run_id)
                 if run.status == 'completed':
                     return True
-            except Exception as e:
-                logger.warning("Could not check run status.", e)
+            except AttributeError:
+                logger.exception("Unable to check run status.")
 
-            await asyncio.sleep(5)
+            await asyncio.sleep(interval)
             retries += 1
 
-        logger.warning("Maximum retries to check run status were reached.")
+        logger.warning("Reached maximum retries to check run status.")
         return False
 
     @log_openai_error
