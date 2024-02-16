e5263633c06a69b11394c8b5e31598d3582a3fbe
MariaBraganca <mcpbbraganca@gmail.com>
2024-02-16 09:29:56 +0100

    add success and failure tests to api interaction

--- auctions_ai/tests/test_chats.py
+++ auctions_ai/tests/test_chats.py
@@ -1,5 +1,6 @@
 import pytest
 import pytest_asyncio
+import openai
 from unittest.mock import AsyncMock, MagicMock, patch
 from auctions_ai.chats import AssistedUserChat
 
@@ -125,13 +126,14 @@ class TestAssistedUserChatFunctionality:
 
 # OpenAI API Interactions
 # -----------------------------------------------------------------------------------------------
-# NOTE:
-# Error handling of openai exceptions are covered on test_decorator.py
 class TestOpenAiInteraction:
     @pytest.mark.asyncio
-    async def test_add_user_message(self, patched_openai, assisted_user_chat):
-        """Tests how to add messages to a thread."""
-        await assisted_user_chat.add_user_message('foo')
+    async def test_add_user_message_for_success(self, patched_openai, assisted_user_chat, caplog):
+        """Tests how to create a message in a thread."""
+        response = await assisted_user_chat.add_user_message('foo')
+
+        assert not caplog.messages
+        assert response == None
 
         patched_openai.beta.threads.messages.create.assert_awaited_once_with(
             thread_id = 2,
@@ -140,9 +142,14 @@ class TestOpenAiInteraction:
         )
 
     @pytest.mark.asyncio
-    async def test_run_assistant(self, patched_openai, assisted_user_chat):
+    async def test_run_assistant_for_success(self, patched_openai, assisted_user_chat, caplog):
         """Tests how to create a run in a thread."""
-        await assisted_user_chat.run_assistant()
+        run = MagicMock()
+        patched_openai.beta.threads.runs.create.return_value = run
+        response = await assisted_user_chat.run_assistant()
+
+        assert not caplog.messages
+        assert response == run
 
         patched_openai.beta.threads.runs.create.assert_awaited_once_with(
             thread_id = 2,
@@ -150,9 +157,14 @@ class TestOpenAiInteraction:
         )
 
     @pytest.mark.asyncio
-    async def test_retrieve_assistant(self, patched_openai, assisted_user_chat):
-        """Tests how to get a run from a thread."""
-        await assisted_user_chat.retrieve_assistant(3)
+    async def test_retrieve_assistant_for_success(self, patched_openai, assisted_user_chat, caplog):
+        """Tests how to retrieve a run from a thread."""
+        run = MagicMock()
+        patched_openai.beta.threads.runs.retrieve.return_value = run
+        response = await assisted_user_chat.retrieve_assistant(3)
+
+        assert not caplog.messages
+        assert response == run
 
         patched_openai.beta.threads.runs.retrieve.assert_awaited_once_with(
             thread_id = 2,
@@ -160,10 +172,79 @@ class TestOpenAiInteraction:
         )
 
     @pytest.mark.asyncio
-    async def test_read_thread_messages(self, patched_openai, assisted_user_chat):
-        """Tests how to get all messages in a thread."""
-        await assisted_user_chat.read_thread_messages()
+    async def test_read_thread_messages_for_success(self, patched_openai, assisted_user_chat, caplog):
+        """Tests how to list all messages in a thread."""
+        thread_messages = MagicMock()
+        patched_openai.beta.threads.messages.list.return_value = thread_messages
+        response = await assisted_user_chat.read_thread_messages()
+
+        assert not caplog.messages
+        assert response == thread_messages
 
         patched_openai.beta.threads.messages.list.assert_awaited_once_with(
             thread_id = 2
         )
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('exception,expected,log_message', [
+        (openai.APIConnectionError(request = MagicMock()), None, "The server could not be reached"),
+        (openai.RateLimitError(message = '', response = MagicMock(), body = None), None, "A 429 status code was received; we should back off a bit."),
+        (openai.APIStatusError(message = '', response = MagicMock(), body = None), None, "Another non-200-range status code was received"),
+    ])
+    async def test_add_user_message_for_failure(self, patched_openai, assisted_user_chat, exception, expected, log_message, caplog):
+        """Tests error handling when creating a message in a thread."""
+        patched_openai.beta.threads.messages.create.side_effect = exception
+        response = await assisted_user_chat.add_user_message('foo')
+
+        assert log_message in caplog.text
+        assert response == expected
+
+        patched_openai.beta.threads.messages.create.assert_awaited_once()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('exception,expected,log_message', [
+        (openai.APIConnectionError(request = MagicMock()), None, "The server could not be reached"),
+        (openai.RateLimitError(message = '', response = MagicMock(), body = None), None, "A 429 status code was received; we should back off a bit."),
+        (openai.APIStatusError(message = '', response = MagicMock(), body = None), None, "Another non-200-range status code was received"),
+    ])
+    async def test_run_assistant_for_failure(self, patched_openai, assisted_user_chat, exception, expected, log_message, caplog):
+        """Tests error handling when creating a run in a thread."""
+        patched_openai.beta.threads.runs.create.side_effect = exception
+        response = await assisted_user_chat.run_assistant()
+
+        assert log_message in caplog.text
+        assert response == expected
+
+        patched_openai.beta.threads.runs.create.assert_awaited_once()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('exception,expected,log_message', [
+        (openai.APIConnectionError(request = MagicMock()), None, "The server could not be reached"),
+        (openai.RateLimitError(message = '', response = MagicMock(), body = None), None, "A 429 status code was received; we should back off a bit."),
+        (openai.APIStatusError(message = '', response = MagicMock(), body = None), None, "Another non-200-range status code was received"),
+    ])
+    async def test_retrieve_assistant_for_failure(self, patched_openai, assisted_user_chat, exception, expected, log_message, caplog):
+        """Tests error handling when retrieving a run from a thread."""
+        patched_openai.beta.threads.runs.retrieve.side_effect = exception
+        response = await assisted_user_chat.retrieve_assistant(3)
+
+        assert log_message in caplog.text
+        assert response == expected
+
+        patched_openai.beta.threads.runs.retrieve.assert_awaited_once()
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize('exception,expected,log_message', [
+        (openai.APIConnectionError(request = MagicMock()), None, "The server could not be reached"),
+        (openai.RateLimitError(message = '', response = MagicMock(), body = None), None, "A 429 status code was received; we should back off a bit."),
+        (openai.APIStatusError(message = '', response = MagicMock(), body = None), None, "Another non-200-range status code was received"),
+    ])
+    async def test_read_thread_messages_for_failure(self, patched_openai, assisted_user_chat, exception, expected, log_message, caplog):
+        """Tests error handling when listing all messages from a thread."""
+        patched_openai.beta.threads.messages.list.side_effect = exception
+        response = await assisted_user_chat.read_thread_messages()
+
+        assert log_message in caplog.text
+        assert response == expected
+
+        patched_openai.beta.threads.messages.list.assert_awaited_once()
