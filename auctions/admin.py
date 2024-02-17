c1cdbb4fc066fcc68c89dd3a01e74e9061ebbd11
MariaBraganca <mcpbbraganca@gmail.com>
2024-02-17 09:21:31 +0100

    remove trailing spaces and no newlines with pre-commit

--- auctions/admin.py
+++ auctions/admin.py
@@ -4,5 +4,5 @@ from auctions.models import Auction
 # Register your models here.
 class AuctionAdmin(admin.ModelAdmin):
     list_display = ['number']
-    
+
 admin.site.register(Auction, AuctionAdmin)
--- auctions/tests/test_views.py
+++ auctions/tests/test_views.py
@@ -7,25 +7,25 @@ pytestmark = pytest.mark.django_db
 
 def test_auction_list_view(client):
     response = client.get(path='/auctions')
-    
+
     assert response.status_code == 200
     assert response.template_name == ['auctions/auction_list.html']
     assert response.is_rendered == True
-    
+
     soup = BeautifulSoup(response.rendered_content, 'html.parser')
     identifiers = [i.text for i in soup.find_all("td", {"id": "auctionNumber"})]
     auction_numbers = list(Auction.objects.values_list('number', flat=True))
-    
+
     assert identifiers == auction_numbers
-    
-def test_auction_detail_view(client):   
+
+def test_auction_detail_view(client):
     auction = Auction.objects.first()
     response = client.get(path=f'/auctions/{auction.pk}')
-    
+
     assert response.status_code == 200
     assert response.template_name == ['auctions/auction_detail.html']
     assert response.is_rendered == True
-    
+
     soup = BeautifulSoup(response.rendered_content, 'html.parser')
     identifier = soup.find("dd", {"id": "auctionNumber"})
     assert identifier.text == auction.number
